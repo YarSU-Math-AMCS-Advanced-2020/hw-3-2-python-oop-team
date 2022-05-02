6656afcf2e201720cd5aa3c42c6dd5cd3fc14c03
yepseeke <yepseeke.kaps@yandex.ru>
2022-05-02 23:57:33 +0300

    Fix class hotel, location

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from database import DB
+from hotel import Hotel
+from location import Location
 
 if __name__ == '__main__':
-    filters = {'city': 'London', 'price': (123, 4123)}
+    filters = {'city': 'Vilnius', 'price': (123, 4123)}
     db = DB('modified_hotels.json')
     a = db.find_hotels(filters)
-    print(a)
+    hotels = []
+    #print(a)
+    for item in a:
+        loc = Location(item['location']['street'], item['location']['district'], item['location']['city'],
+                       item['location']['country'])
+        hotel = Hotel(item['title'], item['price'], loc)
+        hotels.append(hotel)
+    for hotels in hotels:
+        hotels.print_hotel_info()
