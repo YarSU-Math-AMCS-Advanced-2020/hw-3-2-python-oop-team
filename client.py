06e3789df60b4b6d83bca63be4777b4bbc208afc
BuildTools <unconfigured@null.spigotmc.org>
2022-05-20 12:09:36 +0300

    Change attributes in class client

--- client.py
+++ client.py
@@ -1,17 +1,32 @@
 import datetime
+from purchased_hotel import *
+from purchased_ticket import *
+from tour import *
 
 
 class Client:
-    def __init__(self, first_name: str = 'Unknown', last_name: str = 'Shadow',
-                 birthdate: datetime = datetime.date(1970, 1, 1),
-                 mail: str = 'noonecares@hmail.com', password: str = 'qwerty123'):
+    def __init__(self, client_id: str):
+        self.client_id = client_id
+
+        self.first_name = 'Unknown'
+        self.last_name = 'Shadow'
+        self.birthdate = datetime.date(1970, 1, 1)
+        self.mail = 'noonecares@hmail.com'
+        self.phone_number = '+79000000000'
+        self.password = 'qwerty123'
+
+        self.list_of_purchased_hotels: list[PurchasedHotel]
+        self.list_of_purchased_train_tickets: list[PurchasedTrainTicket]
+        self.list_of_purchased_plane_tickets: list[PurchasedPlaneTicket]
+        self.list_of_tours: list[Tour]
+
+    def registration(self, first_name: str, last_name: str, birthdate: datetime, mail: str, phone_number: str,
+                     password: str):
         self.first_name = first_name
         self.last_name = last_name
         self.birthdate = birthdate
         self.mail = mail
+        self.phone_number = phone_number
         self.password = password
 
-    # TODO: getters, setters, phone number
-
-
-
+    # TODO: getters, setters
