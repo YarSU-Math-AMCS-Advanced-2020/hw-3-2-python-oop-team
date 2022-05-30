836bd66bb2feb67457f5a45c3ff307987ff3a3a2
yepseeke <yepseeke.kaps@yandex.ru>
2022-05-30 19:52:21 +0300

    delete booking class

--- booking.py
+++
@@ -1,6 +0,0 @@
-from client import Client
-from decimal import Decimal
-from hotel import Hotel
-from ticket import Ticket, PlaneTicket, TrainTicket, BusTicket
-import datetime
-from typing import Union
