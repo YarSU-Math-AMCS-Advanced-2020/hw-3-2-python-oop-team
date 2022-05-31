738df1c424d189ad4ecfe0bf798be94123c92a9f
bybish <vladchernov77@mail.ru>
2022-05-31 22:38:24 +0300

    fix menu

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ from front_controller import FrontController
 from request import Request
 
 front_controller = FrontController()
+client_id = '1'
 
 fin = easygui.buttonbox(msg='', choices=('Отели', 'Авиа', 'Ж/Д','Туры'), image='index.jpg', title="Авиослейвс ПутешествиЯ")
 while True:
@@ -12,11 +13,13 @@ while True:
         plane_dict = {'city': fin}
         request = Request('find_hotels', plane_dict)
         filter_list = front_controller.handle(request)
+        map_id = dict()
         for item in range(len(filter_list)):
             string ='Название ' + filter_list[item]['title'] + '   Страна ' + filter_list[item]['location']['country'] + '   Город ' + filter_list[item]['location']['city']
-            string = string.ljust(200) + filter_list[item]['id'].rjust(30)
+            map_id[string] = filter_list[item]['id']
             info.append(string)
         out = easygui.multchoicebox(msg="пожалуйста, умоляю, выбери билеты:", title="", choices=info)
-        print(out[0].split()[-1])
-        plane_dict = {'id': out[0].split()[-1]}
-        request = Request('buy_hotel', plane_dict)
+        request = Request('buy_hotel', {'id': map_id[out[0]], 'client_id': client_id})
+        resp = front_controller.handle(request)
+        print(resp)
+        exit()
