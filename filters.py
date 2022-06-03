932c219f4be413deb99c5cf96265673131539c27
yepseeke <yepseeke.kaps@yandex.ru>
2022-06-04 02:50:28 +0300

    add typehint for filters

--- filters.py
+++ filters.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from typing import Dict, Union
+from typing import Dict, Union, Tuple
 
 from purchase import Purchase
 
@@ -15,37 +15,37 @@ class Filters:
 
 
 class HotelFilters(Filters):
-    def __init__(self, hotel_filters: Dict[str, Union[str, Decimal, Dict[str, str]]]):
+    def __init__(self, hotel_filters: Dict[str, Union[str, Decimal, Dict[str, str], Tuple[Decimal]]]):
         super().__init__()
-        if hotel_filters.get('id'):
+        if 'id' in hotel_filters:
             self.filter_list.append(lambda x: x.get('id') == hotel_filters['id'])
-        if hotel_filters.get('city'):
+        if 'city' in hotel_filters:
             self.filter_list.append(lambda x: x.get('location', {}).get('city') == hotel_filters['city'])
-        if hotel_filters.get('country'):
+        if 'country' in hotel_filters:
             self.filter_list.append(lambda x: x.get('location', {}).get('country') == hotel_filters['country'])
-        if hotel_filters.get('price'):
+        if 'price' in hotel_filters:
             self.filter_list.append(lambda x: hotel_filters['price'][0] <= x.get('price') <= hotel_filters['price'][1])
 
 
 class TicketFilters(Filters):
-    def __init__(self, ticket_filters: Dict[str, Union[str, Decimal]]):
+    def __init__(self, ticket_filters: Dict[str, Union[str, Decimal, Tuple[Decimal]]]):
         super().__init__()
-        if ticket_filters.get('id'):
+        if 'id' in ticket_filters:
             self.filter_list.append(lambda x: x.get('id') == ticket_filters['id'])
-        if ticket_filters.get('departure_time'):
+        if 'departure_time' in ticket_filters:
             self.filter_list.append(lambda x: x.get('departure_time') == ticket_filters['departure_time'])
-        if ticket_filters.get('arrival_time'):
+        if 'arrival_time' in ticket_filters:
             self.filter_list.append(lambda x: x.get('arrival_time') == ticket_filters['arrival_time'])
-        if ticket_filters.get('departure_date'):
+        if 'departure_date' in ticket_filters:
             self.filter_list.append(lambda x: x.get('departure_date') == ticket_filters['departure_date'])
-        if ticket_filters.get('arrival_time'):
+        if 'arrival_date' in ticket_filters:
             self.filter_list.append(lambda x: x.get('arrival_date') == ticket_filters['arrival_date'])
-        if ticket_filters.get('duration'):
+        if 'duration' in ticket_filters:
             self.filter_list.append(lambda x: x.get('duration') == ticket_filters['duration'])
-        if ticket_filters.get('price'):
+        if 'price' in ticket_filters:
             self.filter_list.append(
                 lambda x: ticket_filters['price'][0] <= x.get('price') <= ticket_filters['price'][1])
-        if ticket_filters.get('from'):
+        if 'from' in ticket_filters:
             self.filter_list.append(lambda x: x.get('from') == ticket_filters['from'])
-        if ticket_filters.get('to'):
+        if 'to' in ticket_filters:
             self.filter_list.append(lambda x: x.get('to') == ticket_filters['to'])
